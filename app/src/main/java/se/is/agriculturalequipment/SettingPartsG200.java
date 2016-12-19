90338e13d4a50483bdbab6d4dae37a5e525c70d3
iFirst <first.cmu.se@gmail.com>
2016-12-19 11:42:56 +0700

    add activity AddPartG200

--- app/src/main/java/se/is/agriculturalequipment/SettingPartsG200.java
+++ app/src/main/java/se/is/agriculturalequipment/SettingPartsG200.java
@@ -1,13 +1,37 @@
 package se.is.agriculturalequipment;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SettingPartsG200 extends AppCompatActivity {
 
+    private FloatingActionButton fab;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting_parts_g200);
+
+        bindWidget();
+
+//        btnTest.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intentAddPart = new Intent(this, AddPartG200.class);
+//                startActivity(intentAddPart);
+//            }
+//        });
+    }
+
+    public void intentAddPartG200(View view){
+        Intent intentAddPart = new Intent(this, AddPartG200.class);
+        startActivity(intentAddPart);
+    }
+
+    private void bindWidget() {
+        fab = (FloatingActionButton) findViewById(R.id.floatingActionButton);
     }
 }
