9b95ec8cad3ad85327008a9c2a0acfe2e69c4913
iFirst <first.cmu.se@gmail.com>
2017-03-13 23:40:43 +0700

    Added BuyEstimated.class .

--- app/src/main/java/se/is/agriculturalequipment/BuyEstimated.java
+++ app/src/main/java/se/is/agriculturalequipment/BuyEstimated.java
@@ -1,12 +1,17 @@
 package se.is.agriculturalequipment;
 
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.File;
@@ -14,14 +19,26 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class BuyEstimated extends AppCompatActivity {
+
     public static final int REQUEST_CAMERA = 2;
     Uri uri;
+    private ImageView imageView;
+    private EditText edtAmount, edtTest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buy_estimated);
 
+        bindWidget();
+
+    }
+
+    private void bindWidget() {
+        imageView = (ImageView) findViewById(R.id.imgIdentificationNo);
+
+        edtTest = (EditText) findViewById(R.id.edtName);
+        edtAmount = (EditText) findViewById(R.id.edtAmount);
     }
 
     public void takePhoto(View view) {
@@ -34,4 +51,22 @@ public class BuyEstimated extends AppCompatActivity {
         startActivityForResult(Intent.createChooser(intent, "Take a picture with"), REQUEST_CAMERA);
 
     }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+        if(requestCode == REQUEST_CAMERA && resultCode == RESULT_OK){
+//            getContentResolver().notifyChange(uri, null);
+//            ContentResolver cr = getContentResolver();
+//            try {
+//                edtTest.setText(uri.toString());
+//                Bitmap bitmap = MediaStore.Images.Media.getBitmap(cr, uri);
+//                imageView.setImageBitmap(bitmap);
+//                Toast.makeText(getApplicationContext(),uri.getPath(),Toast.LENGTH_SHORT).show();
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            imageView.setImageBitmap(photo);
+        }
+
+    }//End of onActivityResult.
 }
