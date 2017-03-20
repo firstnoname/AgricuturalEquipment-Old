7b264485a6579173504f7cca9c8cd7c4a39cd85d
iFirst <first.cmu.se@gmail.com>
2017-03-20 18:56:37 +0700

    Added BuyEstimated.class .

--- app/src/main/java/se/is/agriculturalequipment/BuyEstimated.java
+++ app/src/main/java/se/is/agriculturalequipment/BuyEstimated.java
@@ -1,13 +1,16 @@
 package se.is.agriculturalequipment;
 
 import android.content.ContentResolver;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -15,13 +18,12 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.File;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class BuyEstimated extends AppCompatActivity {
 
-    public static final int REQUEST_CAMERA = 2;
-    Uri uri;
     private ImageView imageView;
     private EditText edtAmount, edtTest;
 
@@ -41,41 +43,10 @@ public class BuyEstimated extends AppCompatActivity {
         edtAmount = (EditText) findViewById(R.id.edtAmount);
     }
 
-    public void takePhoto(View view) {
-//        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-//        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-//        String imageFileName = "IMG_" + timeStamp + ".jpg";
-//        File f = new File(Environment.getExternalStorageDirectory(), "DCIM/Camera/" + imageFileName);
-//        uri = Uri.fromFile(f);
-//        intent.putExtra(MediaStore.EXTRA_OUTPUT, uri);
-//        startActivityForResult(Intent.createChooser(intent, "Take a picture with"), REQUEST_CAMERA);
 
+    public void dispatchTakePictureIntent(View view) {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_CAMERA);
-        }
 
     }
 
-    public void onActivityResult(int requestCode, int resultCode, Intent data){
-//        if(requestCode == REQUEST_CAMERA && resultCode == RESULT_OK){
-//            getContentResolver().notifyChange(uri, null);
-//            ContentResolver cr = getContentResolver();
-//            try {
-//                Bitmap bitmap = MediaStore.Images.Media.getBitmap(cr, uri);
-//                imageView.setImageBitmap(bitmap);
-//                Toast.makeText(getApplicationContext(),uri.getPath(),Toast.LENGTH_SHORT).show();
-//            } catch (Exception e) {
-//                e.printStackTrace();
-//            }
-//            Bitmap photo = (Bitmap) data.getExtras().get("data");
-//            imageView.setImageBitmap(photo);
-//        }
-
-        if (requestCode == REQUEST_CAMERA && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            imageView.setImageBitmap(imageBitmap);
-        }
-    }//End of onActivityResult.
 }
