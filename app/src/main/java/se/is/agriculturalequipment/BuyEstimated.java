9a28d021a85fe39bdd3f7f199f94f017326cf002
iFirst <first.cmu.se@gmail.com>
2017-04-14 09:33:00 +0700

    Added BuyEstimated.class .

--- app/src/main/java/se/is/agriculturalequipment/BuyEstimated.java
+++ app/src/main/java/se/is/agriculturalequipment/BuyEstimated.java
@@ -2,6 +2,8 @@ package se.is.agriculturalequipment;
 
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.icu.text.SimpleDateFormat;
 import android.net.Uri;
 import android.os.Build;
@@ -57,11 +59,66 @@ public class BuyEstimated extends AppCompatActivity {
         dispatchTakePictureIntent(ACTION_TAKE_PHOTO_B);
     }
 
-    private void dispatchTakePictureIntent(int actionCode){
+    /* Photo album for this application */
+    private String getAlbumName() {
+        return getString(R.string.album_name);
+    }
+
+    private File getAlbumDir() {
+        File storageDir = null;
+
+        if (Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())) {
+
+            storageDir = mAlbumStorageDirFactory.getAlbumStorageDir(getAlbumName());
+            if (storageDir != null) {
+                if (! storageDir.mkdirs()) {
+                    if (! storageDir.exists()){
+                        Log.d("CameraSample", "failed to create directory");
+                        return null;
+                    }
+                }
+            }
+
+        } else {
+            Log.v(getString(R.string.app_name), "External storage is not mounted READ/WRITE.");
+        }
+
+        return storageDir;
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new java.text.SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = JPEG_FILE_PREFIX + timeStamp + "_";
+        File albumF = getAlbumDir();
+
+        File imageF = File.createTempFile(imageFileName, JPEG_FILE_SUFFIX, albumF);
+
+        return imageF;
+    }
+
+    private File setUpPhotoFile() throws IOException {
+        File f = createImageFile();
+
+        mCurrentPhotoPath = f.getAbsolutePath();
+        
+        return f;
+    }
+
+    private void galleryAddPic() {
+        Intent mediaScanIntent = new Intent("android.intent.action.MEDIA_SCANNER_SCAN_FILE");
+        File f = new File(mCurrentPhotoPath);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        this.sendBroadcast(mediaScanIntent);
+    }
+
+    private void dispatchTakePictureIntent(int actionCode) {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        switch (actionCode) {
+        switch(actionCode) {
             case ACTION_TAKE_PHOTO_B:
                 File f = null;
+
                 try {
                     f = setUpPhotoFile();
                     mCurrentPhotoPath = f.getAbsolutePath();
@@ -72,54 +129,69 @@ public class BuyEstimated extends AppCompatActivity {
                     mCurrentPhotoPath = null;
                 }
                 break;
+
             default:
                 break;
-        }
-
+        } // switch
+        
         startActivityForResult(takePictureIntent, actionCode);
     }
 
-    private File setUpPhotoFile() throws IOException{
-        File f = createImageFile();
-        mCurrentPhotoPath = f.getAbsolutePath();
-
-        return f;
-    }
-
-    private File createImageFile() throws IOException{
-        //Create an image file name
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String imageFileName = JPEG_FILE_PREFIX + timeStamp + "_";
-        File albumF = getAlbumDir();
-        File imageF = File.createTempFile(imageFileName, JPEG_FILE_SUFFIX, albumF);
-
-        return imageF;
-    }
-
-    private File getAlbumDir() {
-        File storageDir = null;
-
-        if (Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())) {
-
-            storageDir = mAlbumStorageDirFactory.getAlbumStorageDir(getAlbumName());
-
-            if (storageDir != null) {
-                if (!storageDir.mkdirs()) {
-                    if (!storageDir.exists()) {
-                        Log.d("CameraSample", "failed to create directory");
-
-                        return null;
-                    }
-                }
-            }
-        } else {
-            Log.v("PhotoByIntent", "External storage is not mounted READ/WRITE.");
+    private void handleBigCameraPhoto() {
+        if (mCurrentPhotoPath != null) {
+            setPic();
+            galleryAddPic();
+            mCurrentPhotoPath = null;
         }
 
-        return storageDir;
     }
 
-    public String getAlbumName() {
-        return getString(R.string.album_name);
+    private void setPic() {
+        /* There isn't enough memory to open up more than a couple camera photos */
+		/* So pre-scale the target bitmap into which the file is decoded */
+
+		/* Get the size of the ImageView */
+        int targetW = imageView.getWidth();
+        int targetH = imageView.getHeight();
+
+        /* Get the size of the image */
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
+
+		/* Figure out which way needs to be reduced less */
+        int scaleFactor = 1;
+        if ((targetW > 0) || (targetH > 0)) {
+            scaleFactor = Math.min(photoW/targetW, photoH/targetH);
+        }
+
+		/* Set bitmap options to scale the image decode target */
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+        bmOptions.inPurgeable = true;
+
+		/* Decode the JPEG file into a Bitmap */
+        Bitmap bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+
+		/* Associate the Bitmap to the ImageView */
+        imageView.setImageBitmap(bitmap);
+        imageView.setVisibility(View.VISIBLE);
+
     }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        switch (requestCode) {
+            case ACTION_TAKE_PHOTO_B: {
+                if (resultCode == RESULT_OK) {
+                    handleBigCameraPhoto();
+                }
+                break;
+            } // ACTION_TAKE_PHOTO_B
+        }
+
+    }
+
+   
 }
