185d01b1df778ad670019882c40b22f328852fbe
iFirst <first.cmu.se@gmail.com>
2018-06-08 00:56:39 +0700

    Add guide page.

---
+++ app/src/main/java/se/is/agriculturalequipment/GuideMainMenu.java
@@ -0,0 +1,35 @@
+package se.is.agriculturalequipment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class GuideMainMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guide_main_menu);
+
+    }
+
+    public void intentGuideG200(View view){
+
+    }
+
+    public void intentGuideGX160(View view) {
+
+    }
+
+    public void intentGuideT200(View view) {
+
+    }
+
+    public void intentGuideTM31(View view) {
+
+    }
+
+    public void intentGuideGX35(View view) {
+
+    }
+}
